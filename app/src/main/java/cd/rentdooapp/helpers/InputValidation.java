ac66d5e0cf593b39b8830ed1276ff6e873c66384
PixelByteAG <therealpixelbyte@gmail.com>
2017-10-22 00:08:16 -0400

    Initial commit

---
+++ app/src/main/java/cd/rentdooapp/helpers/InputValidation.java
@@ -0,0 +1,115 @@
+package cd.rentdooapp.helpers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+/**
+ * Created by shuge on 2017-10-21.
+ */
+
+public class InputValidation {
+    private Context context;
+
+    /**
+     * constructor
+     *
+     * @param context
+     */
+    public InputValidation(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * method to check InputEditText filled .
+     *
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    public boolean isInputEditTextFilled(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty()) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+
+
+    /**
+     * method to check InputEditText has valid email .
+     *
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    public boolean isInputEditTextEmail(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    /**
+     * Method to check InputEditText has only numbers
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    //commenting this out for later if we need it, checks to see if input is an integer
+    public boolean isInputEditTextNumbers(TextInputEditText textInputEditText, TextInputLayout  textInputLayout, String message){
+        String value = textInputEditText.getText().toString();
+        textInputLayout.setError(value);
+
+        if(value.matches("[0-9]+") && value.length()  == 10){
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        } else{
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        }
+
+    }
+
+    public boolean isInputEditTextMatches(TextInputEditText textInputEditText1, TextInputEditText textInputEditText2, TextInputLayout textInputLayout, String message) {
+        String value1 = textInputEditText1.getText().toString().trim();
+        String value2 = textInputEditText2.getText().toString().trim();
+        if (!value1.contentEquals(value2)) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText2);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    /**
+     * method to Hide keyboard
+     *
+     * @param view
+     */
+    private void hideKeyboardFrom(View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+}
---
+++ app/src/main/java/cd/rentdooapp/model/User.java
@@ -0,0 +1,56 @@
+package cd.rentdooapp.model;
+
+/**
+ * Created by shuge on 2017-10-21.
+ */
+
+public class User {
+
+    //initialize all variables for user
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String number;
+
+    //initialize all setters and getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNumber(){
+        return number;
+    }
+
+    public void setNumber(String number){
+        this.number = number;
+    }
+}
